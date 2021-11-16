769450d4c13cd9b51e8f7f7ff8f9aadfa8a7a76a
Iliyan <ili.2@abv.bg>
2021-11-16 20:19:56 +0200

    Seventh_commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-  	var card = document.querySelector("card");
+  	var card = document.querySelectorAll("card");
 	setTimeout(function() {
 		card.style="display:none";
 	}, 3000);
